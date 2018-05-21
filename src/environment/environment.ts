2625702b87c3e02f613f559bed7320c687e395ff
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2018-05-21 15:45:46 -0300

    Image picker functionalities

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDLpMJp5BiFq0luAfzvZQRsQSfKUwwCQgw",
+    authDomain: "fir-log-df5ab.firebaseapp.com",
+    databaseURL: "https://fir-log-df5ab.firebaseio.com",
+    projectId: "firebaselog",
+    storageBucket: "firebaselog.appspot.com",
+    messagingSenderId: "1098056656045"
+  }
+}
